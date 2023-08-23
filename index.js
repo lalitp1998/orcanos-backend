4a3c15413b369ed63c3462853fa604d3a24ff16c
unknown <lalit.prajapati@clementius.com>
2023-08-23 15:29:45 +0530

    update api

---
+++ index.js
@@ -0,0 +1,154 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const app = express()
+const port = process.env.PORT || 3000
+app.use(cors());
+app.use(bodyParser.json({ limit: "100mb" }))
+
+
+const axios = require('axios')
+
+app.post('/apiCall', async (req, res) => {
+
+
+
+  let data = {
+    username: "Chrome.Extension",
+    password: "Chrome@2023!"
+  }
+
+  try {
+    let { username, password, url } = req.body;
+
+
+    const token = `${username}:${password}`;
+    const encodedToken = Buffer.from(token).toString('base64');
+    const session_url = `${url}/api/v2/Json/QW_Login`;
+
+    var config = {
+      method: 'post',
+      url: session_url,
+      headers: { 'Authorization': 'Basic ' + encodedToken }
+    };
+
+    let response = await axios(config);
+    console.log(response);
+    if (response.status == 200) {
+      return res.status(200).send({ data: response.data })
+    }
+    else {
+      return res.status(400).send({ data: { HttpCode: 500, message: "Worng Credentials" } })
+    }
+
+
+    //    const token = `username:password`;
+    //     const encodedToken = Buffer.from(token).toString('base64');
+    //     console.log('encodedToken', encodedToken)
+
+  }
+  catch (error) {
+    return res.status(400).send({ data: { HttpCode: 500, message: "Worng Credentials" } })
+  }
+})
+
+app.post('/getObject', async (req, res) => {
+
+  try {
+    let { id, url, username, password } = req.body;
+
+
+    const token = `${username}:${password}`;
+    const encodedToken = Buffer.from(token).toString('base64');
+    let arr = [];
+    for (let i of id) {
+      let session_url = `${url}/api/v2/Json/QW_Get_Object?id=${i}`;
+      var config = {
+        method: 'get',
+        url: session_url,
+        headers: { 'Authorization': 'Basic ' + encodedToken }
+      };
+
+      let response = await axios(config);
+      console.log(response);
+      if (response.status == 200) {
+        arr.push(response.data)
+      }
+    }
+    return res.status(200).send({ data: arr });
+
+
+
+    //    const token = `username:password`;
+    //     const encodedToken = Buffer.from(token).toString('base64');
+    //     console.log('encodedToken', encodedToken)
+
+  }
+  catch (error) {
+    return res.status(400).send({ data: { HttpCode: 500, message: "Worng Credentials" } })
+  }
+})
+// app.post('/getdata',async(req,res)=>{
+//   try{
+//         let { Item_id, Item_Type} = req.body;
+//           const session_url = `${url}/api/v2/Json/QW_Get_Item_Add_Edit`;
+//            var config = {
+//          method: 'get',
+//           url: session_url,
+//     };
+//     let response = await axios(config);
+//       console.log(response);
+//       if (response.status == 200) {
+//         arr.push(response.data)
+//       }
+
+//   }
+//    catch (error) {
+//     return res.status(400).send({ data: { HttpCode: 500, message: "Worng data" } })
+//   }
+// })
+
+
+
+app.post('/getdata', async (req, res) => {
+  
+
+  try {
+    const { Item_id, Item_Type, url, username, password } = req.body;
+    const token = `${username}:${password}`;
+    const encodedToken = Buffer.from(token).toString('base64');
+    const session_url = `${url}/api/v2/Json/QW_Get_Item_Add_Edit`;
+
+    const data = {
+      "Item_id": Item_id,
+      "Item_Type": Item_Type,
+      "VersionId": "",
+      "EditFieldName": "",
+      "CustomerId": ""
+    };
+    const config = {
+      method: 'post',
+      url: session_url,
+      headers: { 'Authorization': 'Basic ' + encodedToken },
+      data: data
+    };
+
+    const response = await axios(config);
+    console.log("RESPONSE",response)
+
+    if (response.status === 200) {
+      return res.status(200).send({ message: 'Data fetched successfully', data: response.data });
+    } else {
+      return res.status(response.status).send({ message: 'Error fetching data' });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ message: 'Internal server error' });
+  }
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
